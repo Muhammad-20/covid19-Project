b24ec7cb075fc873edf424f27ed9e49ed2cb92da
Muhammad Ahmad <Muhammadahmad091011@gmail.com>
2025-06-13 06:59:40 +0300

    Add files via upload

---
+++ COVID19_PROJECT.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import plotly_express as px
+import streamlit as st
+
+st.set_page_config(layout = "wide", page_title = "Covid19 Analysis")
+st.image("https://bourbonnaiscare.com/wp-content/uploads/2020/03/covid-19-pictures-1364x682.jpg")
+
+st.title("Covid19")
+page = st.sidebar.radio('Pages', ['Univariate Analysis', 'MultiVariate Analysis'])
+
+if page == 'Univariate Analysis':
+
+    st.title('Univariate Analysis')
+
+    for col in df.columns:
+        st.plotly_chart(px.histogram(data_frame= df, x= col, title= col))
+    elif page == 'MultiVariate Analysis':
+        st.plotly_chart(px.bar(data_frame=df,x="Jurisdiction_Residence",y= "COVID_deaths"))
+
+df= pd.read_csv("cleaned_df.csv")
+st.dataframe(df)
